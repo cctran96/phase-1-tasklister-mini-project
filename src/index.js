9fe818e80d9da1e655f29be0e4c2f78f14efe5c3
Chau Tran <cctran96@gmail.com>
2021-04-12 22:09:47 -0700

    Added priority

--- src/index.js
+++ src/index.js
@@ -1,24 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
-const form = document.querySelector("#create-task-form")
-const tasks = document.querySelector("#tasks")
-const label = document.querySelector("#new-task-description")
+document.addEventListener('DOMContentLoaded', function(){
+  const form = document.querySelector('#create-task-form')
+  const input = document.querySelector('#new-task-description')
+  const list = document.querySelector('#tasks')
+  const select = document.createElement('select')
+  const option1 = document.createElement('option')
+  const option2 = document.createElement('option')
+  const option3 = document.createElement('option')
 
-function handleSubmit(event) {
-  event.preventDefault()
+  option1.innerText = 'High'
+  option2.innerText = 'Medium'
+  option3.innerText = 'Low'
 
-  const li = document.createElement('li')
-  li.innerText = label.value + " "
-  tasks.appendChild(li)
-  event.target.reset()
-  
-  const button = document.createElement('button')
-  button.innerText = "X"
-  li.appendChild(button)
-  button.addEventListener('click', deleteItem)
-}
+  select.appendChild(option1)
+  select.appendChild(option2)
+  select.appendChild(option3)
+  form.appendChild(select)
 
-function deleteItem(event) {
-  this.parentNode.remove()
-}
-form.addEventListener('submit', handleSubmit) 
-});
+  form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const li = document.createElement('li')
+    const button = document.createElement('button')
+    li.innerText = input.value + ' '
+    button.innerText = 'X'
+    list.appendChild(li).appendChild(button)
+
+    switch(select.value) {
+      case 'High':
+        li.style.color = 'red'
+        break
+      case 'Medium':
+        li.style.color = 'yellow'
+        break
+      default:
+        li.style.color = 'green'
+    }
+
+    button.addEventListener('click', () => list.removeChild(li))
+    form.reset()
+  })
+
+})
